3c3bd704296b4fd07e3e125181bdd37467aa1b78
Herman J. Radtke III <hermanradtke@gmail.com>
2011-08-05 07:36:01 -0700

    Finished widget output

--- GoogleReaderSubscriptions.php
+++ GoogleReaderSubscriptions.php
@@ -31,9 +31,18 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-class GoogleReaderSubscriptions extends WP_Widget {
+/*
+ * TODO custom cache TTL
+ * TODO custom widget title
+ */
 
-    function GoogleReaderSubscriptions() {
+class GoogleReaderSubscriptions extends WP_Widget 
+{
+
+    var $cache_key = 'wp-google-reader-subcriptions';
+
+    function GoogleReaderSubscriptions() 
+    {
         $widget_options = array(
             'description' => __('Display Google Reader subscriptions in sidebar.'), 
             'grsub',
@@ -42,11 +51,103 @@ class GoogleReaderSubscriptions extends WP_Widget {
         parent::WP_Widget('grsub', 'Google Reader Subscriptions', $widget_options);
     }
 
-    function widget( $args, $instance ) {
-        // Widget output
+    function get_subscriptions($email, $password)
+    {
+
+        $subs = get_transient($this->cache_key);
+        if ($subs) {
+            return $subs;
+        }
+
+        $url = "https://www.google.com/accounts/ClientLogin?service=reader&Email={$email}&Passwd={$password}";
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $output = curl_exec($ch);
+
+        if (curl_errno($ch) !== CURLE_OK) {
+            return false;
+        }
+
+        curl_close($ch);
+
+        $parts = explode("\n", $output);
+
+        $auth = array();
+
+        foreach ($parts as $part) {
+            $tmp = explode('=', $part);
+
+            if ($tmp[0] && $tmp[1]) {
+                $auth[$tmp[0]] = $tmp[1];
+            }
+        }
+
+        if (!isset($auth['Auth'])) {
+            return false;
+        }
+
+        $headers = array(
+            "Authorization: GoogleLogin auth={$auth['Auth']}",
+        );
+
+        $ch = curl_init('http://www.google.com/reader/api/0/subscription/list?output=json');
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $subs = curl_exec($ch);
+
+        if (curl_errno($ch) !== CURLE_OK) {
+            return false;
+        }
+
+        curl_close($ch);
+
+
+        $subs = json_decode($subs);
+
+        set_transient($this->cache_key, $subs, 60*60*12);
+
+        return $subs;
     }
 
-    function update($new_instance, $old_instance) {
+    function widget($args, $instance) 
+    {
+        if (!isset($instance['email']) || !isset($instance['password'])) {
+            return;
+        }
+
+        $email = $instance['email'];
+        $password = $this->decrypt_password($instance['password']);
+
+        $subs = $this->get_subscriptions($email, $password);
+
+        if (!$subs) {
+            return;
+        }
+
+        echo '<h3>Blogroll</h3>';
+        echo '<ul class="grsub">';
+
+        foreach ($subs->subscriptions as $sub) {
+
+            $url = esc_attr(substr($sub->id, strlen('feed/')));
+            $title = esc_attr($sub->title);
+
+            echo <<<EOT
+            <li><a href="{$url}" target="_blank">{$title}</a></li>
+EOT;
+        }
+
+        echo '</ul>';
+
+    }
+
+    function update($new_instance, $old_instance) 
+    {
 
         if ($new_instance['email'] && $new_instance['password']) {
             $key = constant('NONCE_KEY');
@@ -60,7 +161,8 @@ class GoogleReaderSubscriptions extends WP_Widget {
         return false;
     }
 
-    function form($instance) {
+    function form($instance) 
+    {
         $email = esc_attr($instance['email']);
 
         if ($instance['password']) {
@@ -94,7 +196,8 @@ class GoogleReaderSubscriptions extends WP_Widget {
     }
 }
 
-function grsub_register_widget() {
+function grsub_register_widget() 
+{
     register_widget('GoogleReaderSubscriptions');
 }
 
